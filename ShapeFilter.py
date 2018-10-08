d491abd9fa8ebadf85bf268ed43b6bdb08d78163
bar-hen <bar.hen.kro@gmail.com>
2018-10-08 17:51:14 +0300

    added filter class for shape

---
+++ ShapeFilter.py
@@ -0,0 +1,18 @@
+from IFilter import IFilter
+import cv2
+
+
+class ShapeFilter(IFilter):
+
+    def __init__(self, edges, epsilon):
+        self.edges = edges
+        self.epsilon = epsilon
+
+    def filter(self, conts):
+        lst = []
+        for cont in conts:
+            epsilon_arc = self.epsilon * cv2.arcLength(cont, True)
+            approx = cv2.approxPolyDP(cont, epsilon_arc, True)
+            if len(approx) == self.edges:
+                lst.append(cont)
+        return lst
