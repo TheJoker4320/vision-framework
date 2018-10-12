7545c5bd36461fa1fef09e9dfaddf1747bc6425c
shaylinzberg <sha97275@gmail.com>
2018-10-12 18:30:56 +0300

    added blur and updated morph

---
+++ Morph.py
@@ -0,0 +1,18 @@
+from IModifier import IModifier
+import cv2
+
+
+class Morph(IModifier):
+
+    def __init__(self, morph_open=None, morph_close=None, use_open=None, use_close=None):
+        self.morph_open = morph_open
+        self.morph_close = morph_close
+        self.use_open = use_open
+        self.use_close = use_close
+
+    def modify(self, frame):
+        if self.use_open:
+            frame = cv2.morphologyEx(frame, cv2.MORPH_OPEN, self.morph_open)
+        if self.use_close:
+            frame = cv2.morphologyEx(frame, cv2.MORPH_CLOSE, self.morph_close)
+        return frame
