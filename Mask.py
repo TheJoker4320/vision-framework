2ea6c9795e3b26a648da47dea4b23b6432a063f5
bar-hen <bar.hen.kro@gmail.com>
2018-10-15 23:07:39 +0300

    added documentaion

--- Mask.py
+++ Mask.py
@@ -2,10 +2,12 @@ from IModifier import IModifier
 import cv2
 
 
-class Mask(IModifier):
-
+class MaskModifier(IModifier):
+    """
+    masks the mask image with the original image by using bitwise and
+    """
     def __init__(self, mask):
         self.mask = mask
 
-    def modify(self, frame):
+    def modify(self, picure):
         return cv2.bitwise_and(frame, frame, mask=self.mask)
