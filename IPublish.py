b2f7723e3ec05ed616116e332e397ef14eafff1a
shaylinzberg <sha97275@gmail.com>
2018-10-16 18:09:26 +0300

    updated the IPublish

--- IPublish.py
+++ IPublish.py
@@ -4,6 +4,7 @@ from abc import ABCMeta, abstractmethod
 class IPublish(object):
     __metaclass__ = ABCMeta
 
+    """ an abstract function that gets a dictionary and sends it to the robot """
     @abstractmethod
-    def publish(self, network_table):
-        raise NotImplementedError
+    def publish(self, dict):
+        pass
