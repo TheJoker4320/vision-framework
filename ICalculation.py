99cfbdb2892a672dd492b3a8f2b18ff24183543a
tzmoz123 <42699543+tzmoz123@users.noreply.github.com>
2018-10-16 18:30:00 +0300

    add a new branch and create the ICalculation

---
+++ ICalculation.py
@@ -0,0 +1,9 @@
+from abc import ABCMeta, abstractmethod
+
+class ICalculation(object):
+    __metaclass__ = ABCMeta
+
+    """ an abstract function that gets a contours and sends a dictionary """
+    @abstractmethod
+    def calc(self, cnt):
+        raise NotImplementedError
