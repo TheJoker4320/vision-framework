704f08598817ba3d2b1a35690344f4607a73fe54
shaylinzberg <sha97275@gmail.com>
2018-10-16 18:26:41 +0300

    updated the IPublish

--- IPublish.py
+++ IPublish.py
@@ -4,7 +4,7 @@ from abc import ABCMeta, abstractmethod
 class IPublish(object):
     __metaclass__ = ABCMeta
 
-    """ an abstract function that gets a dictionary and sends it to the robot """
+    """ an abstract function that gets a data dictionary and publish its data """
     @abstractmethod
     def publish(self, data_dict):
         pass
