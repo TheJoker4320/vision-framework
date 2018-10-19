3019a85454ec557945d4e7cb9c4254b851eb244b
tzmoz <tzmoz123@gmail.com>
2018-10-19 19:50:19 +0300

    general fixes

--- AreaRangeFilter.py
+++ AreaRangeFilter.py
@@ -1,16 +1,17 @@
+from IFilter import IFilter
 import cv2
-from IFilter import IFilter 
-    
+
+
 class AreaRangeFilter(IFilter):
 
-	def __init__(self, MIN_AREA, MAX_AREA):
-		self.MIN_AREA = MIN_AREA
-		self.MAX_AREA = MAX_AREA
-	
-	def filter(self,contours):
-		new_cnt = []
-		for cnt in contours:
-			area = cv2.countourArea(cnt)
-			if MIN_AREA <= area <= MAX_AREA:
-				new_cnt.append(cnt)
-		return new_cnt
+    def __init__(self, min_area, max_area):
+        self.min_area = min_area
+        self.max_area = max_area
+
+    def filter(self, contours):
+        new_cnt = []
+        for cnt in contours:
+            area = cv2.countourArea(cnt)
+            if self.min_area <= area <= self.max_area:
+                new_cnt.append(cnt)
+        return new_cnt
