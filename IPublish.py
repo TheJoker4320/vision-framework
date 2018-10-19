97c7a66bb49fba9ff91ac3e2ae5d341dc6f3936d
Shay <42142645+LilApple@users.noreply.github.com>
2018-10-19 20:16:28 +0300

    Update IPublish.py
    Updated Documentation

--- IPublish.py
+++ IPublish.py
@@ -4,7 +4,10 @@ from abc import ABCMeta, abstractmethod
 class IPublish(object):
     __metaclass__ = ABCMeta
 
-    """ An abstract function that gets a data dictionary and publish its data """
     @abstractmethod
     def publish(self, data_dict):
+        """
+        :param data:
+        :type data:
+        """
         pass
