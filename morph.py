6f2d73c675fca320ac5c42e24a1cd062cdf472dd
Shay <42142645+LilApple@users.noreply.github.com>
2018-10-21 08:27:03 +0300

    Replaced use_open and use_close

--- morph.py
+++ morph.py
@@ -5,15 +5,13 @@ import cv2
 
 class Morph(IModifier):
 
-    def __init__(self, morph_open=None, morph_close=None, use_open=None, use_close=None):
+    def __init__(self, morph_open=None, morph_close=None ):
         self.morph_open = morph_open
         self.morph_close = morph_close
-        self.use_open = use_open
-        self.use_close = use_close
 
     def modify(self, frame):
-        if self.use_open:
+        if self.morph_open > 0:
             frame = cv2.morphologyEx(frame, cv2.MORPH_OPEN, self.morph_open)
-        if self.use_close:
+        if self.morph_open > 0:
             frame = cv2.morphologyEx(frame, cv2.MORPH_CLOSE, self.morph_close)
         return frame
