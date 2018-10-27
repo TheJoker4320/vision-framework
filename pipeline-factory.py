5f452b88868416747ade6d3c555a1a3c9cecf5c6
shaylinzberg <sha97275@gmail.com>
2018-10-27 13:21:06 +0300

    added the create-pipeline function

--- pipeline-factory.py
+++ pipeline-factory.py
@@ -1,4 +1,4 @@
-import cv2
+from pipeline import Pipeline
 
 
 class PipelineFactory(object):
@@ -7,38 +7,47 @@ class PipelineFactory(object):
     calcs = {}
     publish = {}
 
-    """ wip """
+    """ Creates and returns a single pipeline"""
+    def create_pipeline(self):
+        new_pipeline = Pipeline(self.modifiers, self.filters, self.calcs, self.publish)
+        return new_pipeline
+
+    # wip
     def get_modifiers(self, proprties):
         self.modifiers = proprties['modifiers']
 
-    """ wip """
+    # wip
     def get_filters(self, proprties):
         self.modifiers = proprties['filters']
 
-    """ wip """
+    # wip
     def get_calcs(self, proprties):
         self.modifiers = proprties['calcs']
 
-    """ wip """
+    # wip
     def get_publish(self, proprties):
         self.modifiers = proprties['publish']
 
     """A class decorator that registers the modifier in the pipeline factory"""
+
     @staticmethod
     def register_modifier(modifier):
         PipelineFactory.modifiers[modifier.__name__] = modifier
 
     """A class decorator that registers the filter in the pipeline factory"""
+
     @staticmethod
     def register_filter(filter):
         PipelineFactory.modifiers[filter.__name__] = filter
 
     """A class decorator that registers the calculation in the pipeline factory"""
+
     @staticmethod
     def register_calc(calc):
         PipelineFactory.modifiers[calc.__name__] = calc
 
     """A decorator that registers the publisher in the pipeline factory"""
+
     @staticmethod
     def register_publisher(publisher):
         PipelineFactory.modifiers[publisher.__name__] = publisher
