d631a0566afd922474995c0001a2e49768c5bd0f
bar-hen <bar.hen.kro@gmail.com>
2018-11-02 17:35:17 +0200

    changed naming and fixed a mistake

--- distance-calculation.py
+++ distance-calculation.py
@@ -1,6 +1,6 @@
 from ICalculation import ICalculation
 import cv2
-import math
+import utils
 
 
 class DistanceCalculation(ICalculation):
@@ -8,15 +8,20 @@ class DistanceCalculation(ICalculation):
     Calculates the distance between the camera and the object
     """
 
-    def __init__(self, field_of_view,image_width, real_hight):
+    def __init__(self, field_of_view, image_width, real_height):
         self.focal_length = utils.calculate_focal_length(image_width, field_of_view)
-        self.real_hight = real_hight
+        self.real_height = real_height
 
     def calc(self, contour):
-        dic = {}
-        p0, p1, p2, p3 = cv2.boxPoints(rect)
-        h = utils.distance(p0, p1)
-        w = utils.distance(p1, p2)
-        dist = float(self.real_hight) / max(h, w) * self.focal_length
-        dic['distance'] = dist
-        return dist
+        data_dictionary = {}
+
+        rectangle = cv2.minAreaRect(contour)
+        p0, p1, p2, p3 = cv2.boxPoints(rectangle)
+        side1_length = utils.distance(p0, p1)
+        side2_length = utils.distance(p1, p2)
+
+        rectangle_height = max(side1_length, side2_length)
+        distance = float(self.real_height) / rectangle_height * self.focal_length
+        data_dictionary['distance'] = distance
+
+        return data_dictionary
