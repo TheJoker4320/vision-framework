3ebefa513a097202d2de43a81ea9cc45e29b4e94
tzmoz <tzmoz123@gmail.com>
2018-11-02 19:10:44 +0200

    general fixes

--- camera.py
+++ camera.py
@@ -10,7 +10,7 @@ class Camera:
         :param int port: the port that the camera connect too
         """
         self.port = port
-        self.init_string_port = str(self.port)
+        self.initial_string = "v4l2-ctl -d /dev/video" + str(self.port)
 
     def configure_brightness(self, brightness):
         """
@@ -18,7 +18,7 @@ class Camera:
         configure the brightness
         :param brightness: int
         """
-        os.system("v4l2-ctl -d /dev/video" + self.init_string_port + " -c brightness=" + str(brightness))
+        os.system(self.initial_string + " -c brightness=" + str(brightness))
 
     def configure_contrast(self, contrast):
         """
@@ -26,7 +26,7 @@ class Camera:
         configure the contrast
         :param contrast: int
         """
-        os.system("v4l2-ctl -d /dev/video" + self.init_string_port + " -c contrast=" + str(contrast))
+        os.system(self.initial_string + " -c contrast=" + str(contrast))
 
     def configure_saturation(self, saturation):
         """
@@ -34,7 +34,7 @@ class Camera:
         configure the saturation
         :param saturation: int
         """
-        os.system("v4l2-ctl -d /dev/video" + self.init_string_port + " -c saturation=" + str(saturation))
+        os.system(self.initial_string + " -c saturation=" + str(saturation))
 
     def configure_white_balance_temperature_auto(self, white_balance_temperature_auto):
         """
@@ -42,7 +42,7 @@ class Camera:
         configure the auto white balance temperature
         :param white_balance_temperature_auto: int
         """
-        os.system("v4l2-ctl -d /dev/video" + self.init_string_port + " -c white_balance_temperature_auto=" + str(
+        os.system(self.initial_string + " -c white_balance_temperature_auto=" + str(
             white_balance_temperature_auto))
 
     def configure_power_line_frequency(self, power_line_frequency):
@@ -51,8 +51,7 @@ class Camera:
         configure the power line frequency
         :param power_line_frequency: int
         """
-        os.system(
-            "v4l2-ctl -d /dev/video" + self.init_string_port + " -c power_line_frequency=" + str(power_line_frequency))
+        os.system(self.initial_string + " -c power_line_frequency=" + str(power_line_frequency))
 
     def configure_white_balance_temperature(self, white_balance_temperature):
         """
@@ -60,8 +59,7 @@ class Camera:
         configure the white balance temperature
         :param white_balance_temperature: int
         """
-        os.system("v4l2-ctl -d /dev/video" + self.init_string_port + " -c white_balance_temperature=" + str(
-            white_balance_temperature))
+        os.system(self.initial_string + " -c white_balance_temperature=" + str(white_balance_temperature))
 
     def configure_sharpness(self, sharpness):
         """
@@ -69,7 +67,7 @@ class Camera:
         configure the sharpness
         :param sharpness: int
         """
-        os.system("v4l2-ctl -d /dev/video" + self.init_string_port + " -c sharpness=" + str(sharpness))
+        os.system(self.initial_string + " -c sharpness=" + str(sharpness))
 
     def configure_backlight_compensation(self, backlight_compensation):
         """
@@ -77,8 +75,7 @@ class Camera:
         configure the backlight compensation
         :param backlight_compensation: int
         """
-        os.system("v4l2-ctl -d /dev/video" + self.init_string_port + " -c backlight_compensation=" + str(
-            backlight_compensation))
+        os.system(self.initial_string + " -c backlight_compensation=" + str(backlight_compensation))
 
     def configure_exposure_auto(self, exposure_auto):
         """
@@ -86,7 +83,7 @@ class Camera:
         configure the auto exposure
         :param exposure_auto: int
         """
-        os.system("v4l2-ctl -d /dev/video" + self.init_string_port + " -c exposure_auto=" + str(exposure_auto))
+        os.system(self.initial_string + " -c exposure_auto=" + str(exposure_auto))
 
     def configure_exposure_absolute(self, exposure_absolute):
         """
@@ -94,7 +91,7 @@ class Camera:
         configure the absolute exposure
         :param exposure_absolute: int
         """
-        os.system("v4l2-ctl -d /dev/video" + self.init_string_port + " -c exposure_absolute=" + str(exposure_absolute))
+        os.system(self.initial_string + " -c exposure_absolute=" + str(exposure_absolute))
 
     def configure_pan_absolute(self, pan_absolute):
         """
@@ -102,7 +99,7 @@ class Camera:
         configure the absolute pan
         :param pan_absolute: int
         """
-        os.system("v4l2-ctl -d /dev/video" + self.init_string_port + " -c pan_absolute=" + str(pan_absolute))
+        os.system(self.initial_string + " -c pan_absolute=" + str(pan_absolute))
 
     def configure_tilt_absolute(self, tilt_absolute):
         """
@@ -110,7 +107,7 @@ class Camera:
         configure the absolute tilt
         :param tilt_absolute:
         """
-        os.system("v4l2-ctl -d /dev/video" + self.init_string_port + " -c tilt_absolute=" + str(tilt_absolute))
+        os.system(self.initial_string + " -c tilt_absolute=" + str(tilt_absolute))
 
     def configure_zoom_absolute(self, zoom_absolute):
         """
@@ -118,7 +115,7 @@ class Camera:
         configure the absolute zoom
         :param zoom_absolute:
         """
-        os.system("v4l2-ctl -d /dev/video" + self.init_string_port + " -c zoom_absolute=" + str(zoom_absolute))
+        os.system(self.initial_string + " -c zoom_absolute=" + str(zoom_absolute))
 
     def get_frame(self):
         """
