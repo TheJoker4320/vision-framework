018106512f85f988b814e1fbf846f5e6deda930d
Shay <42142645+LilApple@users.noreply.github.com>
2018-11-03 21:19:26 +0200

    general fixes

--- area-filter-ratio.py
+++ area-filter-ratio.py
@@ -2,7 +2,7 @@ import cv2
 from IFilter import IFilter
 import utils
 
-"""Gets the countours and filters them by area ratio range """
+# Gets the countours and filters them by area ratio range
 class AreaRatioFilter(IFilter):
 
     def __init__(self, min_area_ratio, max_area_ratio):
@@ -13,12 +13,12 @@ class AreaRatioFilter(IFilter):
         lst = []
         for cont in conts:
             rect = cv2.minAreaRect(cont)
-            p0, p1, p2, p3 = cv2.boxPoints(rect)
-            h = utils.distance(p0, p1)
-            w = utils.distance(p1, p2)
-            rect_area = w * h
-            area = cv2.contourArea(cnt)
+            point1, point2, point3, point4 = cv2.boxPoints(rect)
+            reg_height = utils.distance(point1, point2)
+            reg_width = utils.distance(point3, point4)
+            rect_area = reg_width * reg_height
+            cont_area = cv2.contourArea(cont)
 
-            if self.max_area_ratio >= float(rect_area) / area >= self.min_area_ratio:
+            if self.max_area_ratio >= float(rect_area) / cont_area >= self.min_area_ratio:
                 lst.append(cont)
         return lst
