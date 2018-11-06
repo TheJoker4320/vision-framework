79ae9ad23757c24d9049b8df2928d91998a0e0f1
shaylinzberg <sha97275@gmail.com>
2018-11-06 21:11:45 +0200

    PEP8 fixes

--- network-table-publisher.py
+++ network-table-publisher.py
@@ -1,20 +1,19 @@
-import cv2
 from IPublish import IPublish
 from networktables import NetworkTables
 
-"""Gets Data and sends it to the smart dashboard"""
+"""Gets Data and, team number and table name sends it to the specified table"""
+
 
 class NetworkTablePublisher(IPublish):
-    table
-    def __init__(self,table_name, team_num):
-        team = str(team)
+
+    def __init__(self, table_name, team_num):
+        team = str(team_num)
         while len(team) != 4:
             team = '0' + team
-        ip = '10.'+ team[:2] + '.' + team[2:] + '.2'
-        NetworkTables.initialize(ip = server)
+        ip = '10.' + team[:2] + '.' + team[2:] + '.2'
+        NetworkTables.initialize(ip)
         self.table = NetworkTables.getTable(table_name)
-    
+
     def publish(self, data):
         for key in data:
             self.table.putNumber(key, data[key])
-
