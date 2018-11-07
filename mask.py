779fbb904bdf0fe1816554be03ab343c58939e45
bar-hen <bar.hen.kro@gmail.com>
2018-11-07 23:45:58 +0200

    PEP8 fixes

--- mask.py
+++ mask.py
@@ -1,11 +1,12 @@
 from IModifier import IModifier
 import cv2
 
- """ Masks the mask image with the original image by using bitwise and """
-class MaskModifier(IModifier):
+""" Masks the mask image with the original image by using bitwise and """
 
+
+class MaskModifier(IModifier):
     def __init__(self, mask):
         self.mask = mask
 
-    def modify(self, picure):
+    def modify(self, frame):
         return cv2.bitwise_and(frame, frame, mask=self.mask)
