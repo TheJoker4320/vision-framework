3d00ce9e934805ba23794d6c38149dcf6871cbf0
tzmoz <tzmoz123@gmail.com>
2018-11-09 19:23:38 +0200

    PEP8 fixes

--- angle-calculation.py
+++ angle-calculation.py
@@ -8,12 +8,14 @@ class AngleCalculation(ICalculation):
     Calculate the x and y angels between the camera and the object in the image
     """
 
-    def __init__(self, image_width, horizontal_field_of_view, image_x_center, image_y_center):
+    def __init__(self, image_width, horizontal_field_of_view, image_x_center,
+                 image_y_center):
         self.image_width = image_width
         self.horizontal_field_of_view = horizontal_field_of_view
         self.image_x_center = image_x_center
         self.image_y_center = image_y_center
-        self.focal_length = utils.calculate_focal_length(self.image_width, self.horizontal_field_of_view)
+        self.focal_length = utils.calculate_focal_length(self.image_width,
+                                                         self.horizontal_field_of_view)
 
     def angle_calc(self, center, image_center):
         angle_radians = math.atan((center - image_center) / self.focal_length)
