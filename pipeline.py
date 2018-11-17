c4535957f86bb4caee20452aceff697f00a2dcae
bar-hen <bar.hen.kro@gmail.com>
2018-11-17 18:03:06 +0200

    handled case where there are no contours and added logging

--- pipeline.py
+++ pipeline.py
@@ -1,10 +1,11 @@
 import cv2
+import logging
 
 
 class Pipeline(object):
     """
         represents a single pipeline
-        responsible for processing image via modifiers,filters,calculations and
+        responsible for processing image via modifiers, filters, calculations and
          publishers
     """
 
@@ -38,16 +39,15 @@ class Pipeline(object):
             frame = modifier.modify(frame)
 
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        _, contours, _ = cv2.findContours(gray_frame, cv2.RETR_TREE,
-                                          cv2.CHAIN_APPROX_SIMPLE)
+        _, contours, _ = cv2.findContours(gray_frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        """
-        f represents a filter (called f to avoid naming it like the built
-        in function filter)
-        """
-        for f in self.filters:
-            contours = f.filter(contours)
+        if len(contours) == 0:
+            return
 
+        for filter_object in self.filters:
+            contours = filter_object.filter(contours)
+
+        Pipeline.__check_contours(contours)
         contour = contours[0]  # the first contour that passed all filters
         """
         this iteration responsible for publishing via different publishers
@@ -56,3 +56,13 @@ class Pipeline(object):
         for calculation in self.calculations:
             for publisher in self.publishers:
                 publisher.publish(calculation.calc(contour))
+
+    @staticmethod
+    def __check_contours(contours):
+        """logs the status of the contour's quantity"""
+        contour_counter = len(contours)
+        if contour_counter == 0:
+            logging.info("there were not found any counter")
+        elif contour_counter == 1:
+            logging.info("single contour was found")
+        logging.info("multiple contours were found")
