1a3bf734d47387ee2e2530b48a3217ef67e0f53f
bar-hen <bar.hen.kro@gmail.com>
2018-11-22 18:44:32 +0200

    merged mask logic to color threshold

--- modifiers/color-threshold.py
+++ modifiers/color-threshold.py
@@ -3,20 +3,30 @@ import cv2
 
 
 class ColorThreshold(Modifier):
-    """Doing threshold color using HSV"""
+    """
+    Doing threshold color using HSV.
+    Can perform mask, via bitwise and operation, between the original frame and the hsv
+    """
 
-    def __init__(self, low_hsv, high_hsv):
+    def __init__(self, low_hsv, high_hsv, do_mask):
         """
 
         :param low_hsv: the lower limit of the HSV range
-        :type low_hsv: int
+        :type low_hsv:
         :param high_hsv: the higher limit of the HSV range
-        :type high_hsv: int
+        :type high_hsv:
+        :param do_mask: flag for performing mask
+        :type do_mask: boolean
         """
+
         self.low_hsv = low_hsv
         self.high_hsv = high_hsv
+        self.do_mask = do_mask
 
     def modify(self, pic):
         hsv = cv2.cvtColor(pic, cv2.COLOR_BGR2HSV)
         hsv = cv2.inRange(hsv, self.low_hsv, self.high_hsv)
+        if self.do_mask:
+            hsv = cv2.bitwise_and(pic, pic, mask=hsv)
+
         return hsv
