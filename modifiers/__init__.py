688c8cadfcb1840ee414a88625121f5085d5d9b7
shaylinzberg <sha97275@gmail.com>
2018-11-24 20:53:02 +0200

    init fixes

--- modifiers/__init__.py
+++ modifiers/__init__.py
@@ -1,9 +1,3 @@
-import os
+import package_utilis
 
-def check_import(file_name):
-    return file_name.endswith('.py') and file_name != '__init__.py' and file_name!='modifier.py'
-
-
-__all__ = [file_name[:-3]  for file_name in os.listdir(os.path.dirname(__file__)) if check_import(file_name)]
-
-
+__all__ = package_utilis.create_all()
