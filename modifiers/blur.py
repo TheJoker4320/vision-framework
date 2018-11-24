d71968bfc36636ddf0ebead49fd7ea33ef136bd3
bar-hen <bar.hen.kro@gmail.com>
2018-11-24 20:37:36 +0200

    converted list to tuple in the __init__

--- modifiers/blur.py
+++ modifiers/blur.py
@@ -10,7 +10,7 @@ class Blur(Modifier):
         :param kernel: bluring kernel
         :type kernel: tuple
         """
-        self.kernel = (kernel[0], kernel[1])
+        self.kernel = tuple(kernel)
 
     def modify(self, pic):
         return cv2.GaussianBlur(pic, self.kernel, 0)
