7e26c769a1e3ffaaa4614dfab9b831b43a1394bf
bar-hen <bar.hen.kro@gmail.com>
2018-11-27 17:55:15 +0200

    changes due to changes in package utils

---
+++ pipeline/__init__.py
@@ -0,0 +1,3 @@
+import package_utilis
+
+__all__ = package_utilis.create_all(__path__)
--- publishers/__init__.py
+++ publishers/__init__.py
@@ -1,3 +1,3 @@
 import package_utilis
 
-__all__ = package_utilis.create_all()
+__all__ = package_utilis.create_all(__path__)
