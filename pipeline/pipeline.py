ef5a7bff20cfc7c096461b67ad4f111565123fb3
bar-hen <bar.hen.kro@gmail.com>
2018-11-27 18:31:52 +0200

    added more logs

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -41,14 +41,15 @@ class Pipeline(object):
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         _, contours, _ = cv2.findContours(gray_frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        if len(contours) == 0:
+        logging.info("post modifying")
+        if not Pipeline.__contain_contour(contours):
             return
 
         for filter_object in self.filters:
             contours = filter_object.filter(contours)
 
-        Pipeline.__check_contours(contours)
-        if len(contours) == 0:
+        logging.info("post filtering")
+        if not Pipeline.__contain_contour(contours):
             return
         contour = contours[0]  # the first contour that passed all filters
         """
@@ -60,11 +61,16 @@ class Pipeline(object):
                 publisher.publish(calculation.calc(contour))
 
     @staticmethod
-    def __check_contours(contours):
-        """logs the status of the contour's quantity"""
-        contour_counter = len(contours)
-        if contour_counter == 0:
+    def __contain_contour(contours):
+        """
+        logs the status of the contour's quantity
+        return if the amount of contours greater than 0
+        """
+        contour_amount = len(contours)
+        if contour_amount == 0:
             logging.info("there were not found any counter")
-        elif contour_counter == 1:
+            return False
+        elif contour_amount == 1:
             logging.info("single contour was found")
         logging.info("multiple contours were found")
+        return True
