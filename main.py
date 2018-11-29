02918ecd7bcd39e027967181947690024441f077
bar-hen <bar.hen.kro@gmail.com>
2018-11-29 23:56:57 +0200

    merged camera into the project

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import json
-import cv2
 from pipeline.pipeline_factory import PipelineFactory
 import logging
+from camera import Camera
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
+    camera = Camera(1)
     with open("jak.json", "r") as f:
         prop = json.load(f)
     my_pipeline = PipelineFactory.create_pipeline(prop)
-    image = cv2.imread('test.jpg')
     while True:
-        my_pipeline.process_image(image)
+        my_pipeline.process_image(camera.get_frame())
 
 
 if __name__ == "__main__":
