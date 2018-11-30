b4b06b345fa3994c7388da935c284c3e34ea00ba
bar-hen <bar.hen.kro@gmail.com>
2018-11-30 02:19:06 +0200

    general fixes

--- publishers/logger.py
+++ publishers/logger.py
@@ -4,5 +4,5 @@ from publish import Publish
 
 class Logger(Publish):
     def publish(self, data_dict):
-        for key, value in data.iteritems():
+        for key, value in data_dict.iteritems():
             logging.info("{key}:{value}".format(key=key, value=value))
