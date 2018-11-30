8a7cfdb4de08e16f50d195b55e176fb19daafbcc
bar-hen <bar.hen.kro@gmail.com>
2018-11-30 13:41:07 +0000

    added gui tests in comments

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import json
 from pipeline.pipeline_factory import PipelineFactory
 import logging
 from camera import Camera
+import cv2
 
 
 def main():
-    logging.basicConfig(filename='logger.log', level=logging.INFO)
+    logging.basicConfig(level=logging.INFO)
+    # cv2.namedWindow("screen1")
+    # cv2.namedWindow("screen2")
 
     with open("properties.json", "r") as file_handler:
         properties = json.load(file_handler)
@@ -13,9 +16,11 @@ def main():
 
     camera_settings = properties['camera settings']
     camera = Camera(camera_settings['id'])
-    Camera.camera_setting_setter(camera_settings)
+    camera.camera_setting_setter(camera_settings)
     while True:
-        my_pipeline.process_image(camera.get_frame())
+	frame= camera.get_frame()
+	# cv2.imshow('screen1',frame)
+        my_pipeline.process_image(frame)
 
 
 if __name__ == "__main__":
