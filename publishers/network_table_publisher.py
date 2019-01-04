ab7f0e8f4af15d2264cdf213644d1d95459e56f2
tzmoz <tzmoz123@gmail.com>
2019-01-04 20:08:47 +0200

    general fixes

--- publishers/network_table_publisher.py
+++ publishers/network_table_publisher.py
@@ -7,11 +7,11 @@ class NetworkTablePublisher(Publish):
     Gets Data ,team number and table name.
     Sends it to the specified table via network tables protocol.
     """
-    ip_template = "10.{0}.{1}.2"
+    IP_TEMPLATE = "10.{0}.{1}.2"
 
     def __init__(self, table_name, team_num):
         team_num = '0' * (4 - len(str(team_num))) + str(team_num)
-        ip = NetworkTablePublisher.ip_template.format(team_num[:2], team_num[2:])
+        ip = NetworkTablePublisher.IP_TEMPLATE.format(team_num[:2], team_num[2:])
         NetworkTables.initialize(ip)
         self.table = NetworkTables.getTable(table_name)
 
