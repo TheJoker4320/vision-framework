eda5a29582a886858dec2f1c9ea3dfafc6a52c48
bar-hen <bar.hen.kro@gmail.com>
2019-01-10 18:11:22 +0200

    added ordered dictionery to the properties loading

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import collections
 
 from camera import Camera
 from pipeline.pipeline_factory import PipelineFactory
@@ -9,7 +10,7 @@ def main():
     logging.basicConfig(level=logging.INFO)
 
     with open("properties.json", "r") as file_handler:
-        properties = json.load(file_handler)
+        properties = json.load(file_handler, object_pairs_hook=collections.OrderedDict)
     my_pipeline = PipelineFactory.create_pipeline(properties)
 
     camera_settings = properties['camera settings']
