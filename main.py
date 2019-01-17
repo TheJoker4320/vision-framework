e0a25318350ee481f37c2eafb0ca10e2958799b8
bar-hen <bar.hen.kro@gmail.com>
2019-01-17 15:46:04 +0000

    workshop fixes

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ from pipeline.pipeline_factory import PipelineFactory
 def main():
     logging.basicConfig(level=logging.INFO)
 
-    with open("properties.json", "r") as file_handler:
+    with open("examples/example.json", "r") as file_handler:
         properties = json.load(file_handler, object_pairs_hook=collections.OrderedDict)
     my_pipeline = PipelineFactory.create_pipeline(properties)
 
     camera_settings = properties['camera settings']
     camera = Camera(camera_settings['id'])
-    camera.camera_setting_setter(camera_settings)
+    camera.set_camera_settings(camera_settings)
     while True:
         frame = camera.get_frame()
         my_pipeline.process_image(frame)
