da64b7776dfc9fe0b6809b010ed3aba0e88ed61a
shaylinzberg <sha97275@gmail.com>
2019-01-18 22:26:19 +0200

    test version

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 import json
 import logging
 import collections
+from threading import Thread
 from streamer import Streamer
+import cv2
 
 from camera import Camera
 from pipeline.pipeline_factory import PipelineFactory
 
 
 def main():
-    logging.basicConfig(level=logging.INFO)
 
-    with open("properties.json", "r") as file_handler:
+    logging.basicConfig(level=logging.INFO)
+    feed = Streamer()
+    with open("examples/example.json", "r") as file_handler:
         properties = json.load(file_handler, object_pairs_hook=collections.OrderedDict)
     my_pipeline = PipelineFactory.create_pipeline(properties)
 
-    camera_settings = properties['camera settings']
-    camera = Camera(camera_settings['id'])
-    camera.camera_setting_setter(camera_settings)
+    # camera_settings = properties['camera settings']
+    # camera = Camera(camera_settings['id'])
+    # camera.camera_setting_setter(camera_settings)
+
+    Thread(target=feed.run).start()
+
     while True:
-        frame = camera.get_frame()
-        processed_frame = my_pipeline.process_image(frame)
-        Streamer.update(frame, processed_frame)
+        frame = cv2.imread('test.jpg')
+        # processed_frame = my_pipeline.process_image(frame)
+        feed.update(frame)
 
 
 if __name__ == "__main__":
--- streamer.py
+++ streamer.py
@@ -4,17 +4,15 @@ import cv2
 
 class Streamer(object):
     def __init__(self):
-        self.original_frame = 0  # add black image
-        self.processed_frame = 0  # its better
+        self.original_frame = None
         self.app = Flask("Streamer")
         self.app.add_url_rule("/feed", "feed", self._get_response)
 
     def run(self):
         self.app.run(host='0.0.0.0', port=80, debug=False, threaded=True)
 
-    def update(self, original_frame, processed_frame):
+    def update(self, original_frame):
         self.original_frame = original_frame
-        self.processed_frame = processed_frame
 
     @staticmethod
     def _get_img_bytes(img):
@@ -24,9 +22,8 @@ class Streamer(object):
     def _get_page(self):
         while True:
             frame = Streamer._get_img_bytes(self.original_frame)
-            processed = Streamer._get_img_bytes(self.processed_frame)
             yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + processed + b'\r\n')
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
     def _get_response(self):
         return Response(self._get_page(), mimetype='multipart/x-mixed-replace; boundary=frame')
