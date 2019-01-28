895fcfdee97c849b6c3bcea370a184a5a9267bbe
JokerWorkshopoUser <frcthejoker4320@gmail.com>
2019-01-28 15:40:39 +0200

    convert int to string in dict

--- remote_tuner.py
+++ remote_tuner.py
@@ -20,7 +20,6 @@ class RemoteTuner(object):
 
     def __write_initial(self, name, dic):
         root_table = NetworkTables.getTable(name)
-
         for item in dic.iteritems():
             self.__recursive_write(item[0], item[1], root_table)
 
@@ -39,18 +38,30 @@ class RemoteTuner(object):
             table.putValue(key, value)
 
         else:
+            if type(value) == list and type(value[0]) == int:
+                value = self.__convert_type(value,str)
             sub_table = table.getSubTable(key)
             sub_table.addEntryListener(self.__listener)
             for sub_item in value.iteritems():
                 self.__recursive_write(sub_item[0], sub_item[1], sub_table)
 
+    def __convert_type(self,value,type_to):
+        strings = []
+        for v in value:
+            strings.append(type_to(v))
+        return strings
+    
     def __listener(self, table, key, value, is_new):
         path = table.path
         path_list = path.split('/')[2:]
         current_dictionary = self.pipeline_configurations
         for dictionary_name in path_list:
             current_dictionary = current_dictionary[dictionary_name]
-        current_dictionary[key] = value
+        
+        if type(current_dictionary) == list and type(current_dictionary[key] == str):
+            current_dictionary[key] = int(current_dictionary[key])
 
+            
+        current_dictionary[key] = value
         with open(self.json_file_name, "w") as file_handler:
             json.dump(self.pipeline_configurations, file_handler, indent=0)
