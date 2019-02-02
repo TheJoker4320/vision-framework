1de473eb63eb4dede4c7b23291aa68a3b8ebf7b2
tzmoz <tzmoz123@gmail.com>
2019-02-02 23:35:05 +0200

    general fixes

--- calculations/turn_calculation.py
+++ calculations/turn_calculation.py
@@ -16,23 +16,21 @@ class TurnCalculation(Calculation):
         self.tape_width = tape_width
 
     def calc(self, contours):
-        cntr1 = contours[0]
-        cntr2 = contours[1]
+        if len(contours) == 2:
+            contour1 = contours[0]
+            contour2 = contours[1]
+            distanceCalculation = distance_calculation(self.field_of_view, self.image_width, self.real_height)
+            disFromContour1 = distanceCalculation.calc(contour1)['distance']
+            disFromContour2 = distanceCalculation.calc(contour2)['distance']
+            height = min(disFromContour1, disFromContour2)
 
-        disFromP1toP2 = distance_calculation(self.field_of_view, self.image_width, self.real_height).calc(cntr1)['distance']
-        disFromP1toP3 = distance_calculation(self.field_of_view, self.image_width, self.real_height).calc(cntr2)['distance']
-        height1 = max(disFromP1toP2, disFromP1toP3)
+            merge_contours = calculation_utils.merge_contours(contour1, contour2)
+            disFromCenter = distanceCalculation.calc(merge_contours)['distance']
 
-        disFromP1toP2 = distance_calculation.distance(contour2[0], contour2[1])
-        disFromP1toP3 = distance_calculation.distance(contour2[0], contour2[2])
-        height2 = max(disFromP1toP2, disFromP1toP3)
-
-        height = min(height1, height2)
-        merge_contours = calculation_utils.merge_contours(contour1, contour2)
-        disFromCenter = distance_calculation(self.field_of_view, self.image_width, self.real_height).calc(merge_contours)['distance']
-
-        angle = math.acos((math.pow(disFromCenter, 2) + math.pow(self.tape_width, 2) - math.pow(height, 2))/(2 * disFromCenter * self.tape_width))
-        before_turn = disFromCenter*math.cos(angle)
-        after_turn = disFromCenter*math.sin(angle)
-        dictionary = {"before turn": before_turn, "after turn": after_turn, "angle": angle*180/math.pi}
-        return dictionary
+            angle = math.acos((math.pow(disFromCenter, 2) + math.pow(self.tape_width, 2) - math.pow(height, 2))/(2 * disFromCenter * self.tape_width))
+            before_turn = disFromCenter*math.cos(angle)
+            after_turn = disFromCenter*math.sin(angle)
+            dictionary = {"before turn": before_turn, "after turn": after_turn, "angle": angle*180/math.pi}
+            return dictionary
+        else:
+            return
