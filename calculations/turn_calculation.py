2d2f7fcb288c510c9ad26b674d0566e9964d2005
bar-hen <bar.hen.kro@gmail.com>
2019-02-03 23:56:03 +0200

    general fixes

--- calculations/turn_calculation.py
+++ calculations/turn_calculation.py
@@ -20,12 +20,12 @@ class TurnCalculation(Calculation):
             contour1 = contours[0]
             contour2 = contours[1]
             distanceCalculation = DistanceCalculation(self.field_of_view, self.image_width, self.real_height)
-            disFromContour1 = distanceCalculation.calc(contour1)['distance']
-            disFromContour2 = distanceCalculation.calc(contour2)['distance']
+            disFromContour1 = distanceCalculation.calc([contour1])['distance']
+            disFromContour2 = distanceCalculation.calc([contour2])['distance']
             shorter_distance = min(disFromContour1, disFromContour2)
 
-            merge_contours = calculation_utils.merge_contours(contour1, contour2)
-            disFromCenter = distanceCalculation.calc(merge_contours)['distance']
+            merge_contours = calculation_utils.merge_contours([contour1, contour2])
+            disFromCenter = distanceCalculation.calc([merge_contours])['distance']
 
             angle = math.acos((math.pow(disFromCenter, 2) + math.pow(self.tape_width, 2) - math.pow(shorter_distance, 2)) / (2 * disFromCenter * self.tape_width))
             before_turn = disFromCenter * math.cos(angle)
