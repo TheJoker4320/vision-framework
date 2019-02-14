3ea62ff8da1b1e1b591a512d316010c5b408e55f
bar-hen <bar.hen.kro@gmail.com>
2019-02-14 16:58:48 +0200

    Jetson fixes

--- main.py
+++ main.py
@@ -16,16 +16,16 @@ def main():
         properties = json.load(file_handler, object_pairs_hook=collections.OrderedDict)
     my_pipeline = PipelineFactory.create_pipeline(properties)
 
-    # camera_settings = properties['camera settings']
-    # camera = Camera(camera_settings['id'])
-    # camera.set_camera_settings(camera_settings)
+    camera_settings = properties['camera settings']
+    camera = Camera(camera_settings['id'])
+    camera.set_camera_settings(camera_settings)
 
     feed = Streamer()
 
     Thread(target=feed.run).start()
-    frame = cv2.imread('diagonal_test.jpg')
+    # frame = cv2.imread('diagonal_test.jpg')
     while True:
-        # frame = camera.get_frame()
+        frame = camera.get_frame()
         processed_frame = my_pipeline.process_image(frame)
         feed.update(processed_frame)
 
