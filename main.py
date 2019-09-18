235c597d6d8796a5e3c28dafc2e78bfdac8e5bc9
shaylinzberg <sha97275@gmail.com>
2019-09-18 20:05:06 +0300

    Fixed main PEP8 issues

--- main.py
+++ main.py
@@ -17,20 +17,20 @@ def main():
     with open("examples/example.json", "r") as file_handler:
         properties = json.load(file_handler, object_pairs_hook=collections.OrderedDict)
     my_pipeline = PipelineFactory.create_pipeline(properties)
-    
+
     camera_settings = properties['camera settings']
     camera = Camera(camera_settings['id'], cv2.CAP_V4L)
-    #camera.set_camera_settings(camera_settings)
-    
+    # camera.set_camera_settings(camera_settings)
+
     NetworkTables.initialize(server='10.43.20.69')
     # frame = cv2.imread('ball.jpg')
-    
+
     # frame = cv2.imread('diagonal_test.jpg')
-    
+
     while True:
         frame = camera.get_frame()
         processed_frame = my_pipeline.process_image(frame)
 
-    
+
 if __name__ == "__main__":
     main()
