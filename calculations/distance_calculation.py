7069fb2c167eba1768f6745af515ee9dbc41ef02
shaylinzberg <sha97275@gmail.com>
2019-09-18 20:03:10 +0300

    Created the new distance calculation

--- calculations/distance_calculation.py
+++ calculations/distance_calculation.py
@@ -6,29 +6,24 @@ import calculation_utils
 class DistanceCalculation(Calculation):
     """
     Calculates the distance between the camera and the object
-    Uses the image's width, camera's field of view and the object's height in reality.
+    Uses self made distance calculation function
     """
 
-    def __init__(self, field_of_view, image_width, real_height):
-        """
-        :param field_of_view:
-        :param image_width:
-        :param real_height:
-        """
-        self.focal_length = calculation_utils.calculate_focal_length(image_width, field_of_view)
-        self.real_height = real_height
-
     def calc(self, contours):
-        data_dictionary = {}
-
+        data_dict = {}
         merged_cont = calculation_utils.merge_contours(contours)
         rectangle = cv2.minAreaRect(merged_cont)
-        p0, p1, p2, p3 = cv2.boxPoints(rectangle)
-        side1_length = calculation_utils.distance(p0, p1)
-        side2_length = calculation_utils.distance(p1, p2)
+        _, p1, p2, _ = cv2.boxPoints(rectangle)
+        height = calculation_utils.distance(p1, p2)
+        distance = self.distance_calc(height)
+        data_dict['distance'] = distance
+        return data_dict
 
-        rectangle_height = max(side1_length, side2_length)
-        distance = float(self.real_height) / rectangle_height * self.focal_length
-        data_dictionary['distance'] = distance
-
-        return data_dictionary
+    def distance_calc(self, height):
+        """
+        WIP
+        :param height: The height of the given object
+        :return: The distance between the camera and the object
+        """
+        distance = height
+        return distance
