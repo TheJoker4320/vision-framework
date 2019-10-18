fe53f45a9e3b178c5de5abbefb58712845bc6c18
tzmoz <tzmoz123@gmail.com>
2019-10-18 21:25:05 +0300

    general fixes and calculation fixes

--- calculations/distance_calculation.py
+++ calculations/distance_calculation.py
@@ -7,17 +7,24 @@ class DistanceCalculation(Calculation):
     """
     Calculates the distance between the camera and the object
     Uses self made distance calculation function
+         where y is the real distance of the camera from the object
+         and x is the image's height in pixels (imaginary_height)
     """
 
-    def __init__(self, calc_constant):
-        self.calc_constant = calc_constant
-
+    def __init__(self, distance_function):
+        """
+        :param distance_function:
+        """
+        self.distance_function = distance_function
+        
     def calc(self, contours):
-        data_dict = {}
+        data_dictionary = {}
+        
         merged_cont = calculation_utils.merge_contours(contours)
         rectangle = cv2.minAreaRect(merged_cont)
         _, p1, p2, _ = cv2.boxPoints(rectangle)
         imaginary_height = calculation_utils.distance(p1, p2)
-        distance = self.calc_constant / imaginary_height
-        data_dict['distance'] = distance
-        return data_dict
+        distance = eval(distance_function)
+        data_dictionary['distance'] = distance
+        
+        return data_dictionary
