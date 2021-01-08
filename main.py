3323e3bb7a4c823e422fc747197a7479c218159c
tzmoz <tzmoz123@gmail.com>
2021-01-08 14:50:01 +0200

    fix the main to work

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from camera import Camera
 
 from pipeline.pipeline_factory import PipelineFactory
 from networktables import NetworkTables
+from remote_tuner import RemoteTuner
 
 
 def main():
@@ -17,20 +18,23 @@ def main():
     with open("examples/example.json", "r") as file_handler:
         properties = json.load(file_handler, object_pairs_hook=collections.OrderedDict)
     my_pipeline = PipelineFactory.create_pipeline(properties)
-    
+
     camera_settings = properties['camera settings']
     camera = Camera(camera_settings['id'], cv2.CAP_V4L)
-    #camera.set_camera_settings(camera_settings)
-    
-    NetworkTables.initialize(server='10.43.20.69')
+    camera.set_camera_settings(camera_settings)
+    # tuner = RemoteTuner("examples/example.json", my_pipeline)
+    # NetworkTables.initialize(server='10.43.20.69')
     # frame = cv2.imread('ball.jpg')
-    
-    # frame = cv2.imread('diagonal_test.jpg')
-    
+    streamer = Streamer("examples/example.json")
+    Thread(target=Streamer.run).start()
+    # frame = cv2.imread('test.jpg')
+
     while True:
         frame = camera.get_frame()
         processed_frame = my_pipeline.process_image(frame)
+        streamer.update(processed_frame)
+        # my_pipeline = tuner.get_pipeline()
 
-    
+
 if __name__ == "__main__":
     main()
