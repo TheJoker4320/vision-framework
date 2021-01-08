fix the calculation and add documentation

calculations/distance_calculation_by_vector.py
@@ -1,90 +1,100 @@
 from calculations.calculation import Calculation
 import cv2
+import math
+import logging
 import numpy as np
 import calculation_utils
 
 
 class DistanceCalculationByVector(Calculation):
     """
-    Calculates the distance between the camera and the object
-    Uses the image's width, camera's field of view and the object's height in reality
+    Calculates the distance between the camera and the object across the x, y and z axises
+    Uses the image's width and height, camera's field of views, the object's surface area in reality and the camera's
+    location relative to the robot
     """
 
-    def __init__(self, field_of_view, image_width, image_height, object_surface_area, yaw_angle=0, pitch_angle=0,
-                 roll_angle=0, x_offset=0, y_offset=0, z_offset=0):
+    def __init__(self, horizontal_field_of_view, vertical_field_of_view, image_width, image_height, object_surface_area,
+                 yaw_angle=0, pitch_angle=0, roll_angle=0, x_offset=0, y_offset=0, z_offset=0):
         """
-        :param field_of_view:
-        :param image_width:
-        offsets are the distance in each axis away from the center of the robot
-        yaw_angle is the clockwise yaw angle (in radians) in which the camera is rotated, the yaw angle is the angle around the y axis,
-        pitch_angle is the clockwise pitch angle (in radians) in which the camera is rotated, the pitch angle is the angle around the x axis,
-        roll_angle is the clockwise roll angle (in radians) in which the camera is rotated, the roll angle is the angle around the z axis
+        :param horizontal_field_of_view: the field of view on the horizontal axis (in degrees)
+        :type horizontal_field_of_view: float
+
+        :param vertical_field_of_view: the field of view on the vertical axis (in degrees)
+        :type vertical_field_of_view: float
+
+        :param image_width: the width of the image (in pixels)
+        :type image_width: int
+
+        :param image_height: the height of the image (in pixels)
+        :type image_height: int
+
+        :param yaw_angle: the clockwise yaw angle (in degrees) in which the camera is rotated, the yaw angle is the
+                          angle around the y axis (default is 0.0)
+        :type yaw_angle: float
+        :param pitch_angle: the clockwise pitch angle (in degrees) in which the camera is rotated, the yaw angle is the
+                            angle around the x axis (default is 0.0)
+        :type pitch_angle: float
+        :param roll_angle: the clockwise roll angle (in degrees) in which the camera is rotated, the roll angle is the
+                           angle around the z axis (default is 0.0)
+        :type roll_angle: float
+
+
+        :param x_offset: the distance (in meters) in the x axis away from the center of the robot (default is 0.0)
+        :type x_offset: float
+        :param y_offset: the distance (in meters) in the y axis away from the center of the robot (default is 0.0)
+        :type y_offset: float
+        :param z_offset: the distance (in meters) in the z axis away from the center of the robot (default is 0.0)
+        :type z_offset: float
         """
-        self.field_of_view = field_of_view
+        self.horizontal_field_of_view = horizontal_field_of_view
+        self.vertical_field_of_view = vertical_field_of_view
         self.image_width = image_width
         self.image_height = image_height
-        self.object_surface_area = object_surface_area
-        self.focal_length = calculation_utils.calculate_focal_length(image_width, field_of_view)
-        self.yaw_angle = yaw_angle
-        self.pitch_angle = pitch_angle
-        self.roll_angle = roll_angle
-        self.x_offset = x_offset
-        self.y_offset = y_offset
-        self.z_offset = z_offset
+        self.sqrt_object_area = math.sqrt(object_surface_area)
+        self.focal_length = calculation_utils.calculate_focal_length(image_width, horizontal_field_of_view)
+
+        self.offset = np.array([x_offset, y_offset, z_offset])
+        self.rotation_matrix = calculation_utils.create_rotation_matrix(np.radians(yaw_angle), np.radians(pitch_angle),
+                                                                        np.radians(roll_angle))
 
     def calc(self, contours):
         data_dictionary = {}
 
+        # merge all contours and refers to them as one
         merged_cont = calculation_utils.merge_contours(contours)
-        cont_area = cv2.contourArea(merged_cont)
-        x_object_center, y_object_center = calculation_utils.find_center(contours)
 
-        x_frame_center = self.image_width / 2
-        y_frame_center = self.image_height / 2
+        # find the merged-contour's square root (in pixels)
+        sqrt_cont_area = math.sqrt(cv2.contourArea(merged_cont))
 
-        x_vector = x_frame_center - x_object_center
-        y_vector = y_frame_center - y_object_center
+        # find the merge-contour-center's coordinates
+        x_cont_center, y_cont_center = calculation_utils.find_center(merged_cont)
+        # find the frame-center's coordinates
+        x_frame_center, y_frame_center = self.image_width / 2, self.image_height / 2
 
-        alpha = x_vector * self.focal_length / x_frame_center
-        beta = y_vector * self.focal_length / y_frame_center
+        # find x and y vectors from frame-center's coordinates to the merge-contour-center's coordinates
+        x_vector, y_vector = x_cont_center - x_frame_center, y_cont_center - y_frame_center
 
-        # the norm of the vector between the camera and the object (in meters)
-        distance_vector = self.focal_length * self.object_surface_area / cont_area
+        # find the horizontal angle that from frame-center to merge-contour-center
+        horizontal_angle = x_vector * self.horizontal_field_of_view / x_frame_center
+        # find the vertical angle that from frame-center to merge-contour-center
+        vertical_angle = y_vector * self.vertical_field_of_view / y_frame_center
 
-        rel = np.array([[np.sin(alpha), np.sin(beta),
-                         np.sqrt(1 - np.sin(alpha) ** 2 - np.sin(beta) ** 2)]]) * distance_vector
+        # find the distance vector from the object (in meters)
+        distance_vector = self.focal_length * self.sqrt_object_area / sqrt_cont_area
 
-        offset = np.array([self.x_offset, self.y_offset, self.z_offset])
-        rotation_matrix = self.create_rotation_matrix()
+        # find the x,y and z distance from the object (in meters)
+        rel = np.array([[np.sin(horizontal_angle), np.sin(vertical_angle),
+                         np.sqrt(1 - np.sin(horizontal_angle) ** 2 - np.sin(vertical_angle) ** 2)]]) * distance_vector
 
-        """
-        normal_distance_vector = self.focal_length*math.sqrt(object_surface_area/cont_area)
+        # perform rotation matrix on the distance and add the given offset
+        distance = self.rotation_matrix.dot(rel.T).flatten() + self.offset
 
-        distance_from_image_center_x = int(self.image_width)/2 - x_center
-        distance_from_image_center_y = int(self.image_height)/2 - y_center
-        distance_x = normal_distance_vector * math.sin(self.field_of_view)*(2)
-        distance_y = normal_distance_vector * math.sin(self.field_of_view)*(2/self.image_width)
+        data_dictionary['distance_vector_x'] = str(distance[0])
+        data_dictionary['distance_vector_y'] = str(distance[1])
+        data_dictionary['distance_vector_z'] = str(distance[2])
 
-        # x^2 + y^2 + z^2 = normal_distance_vector^2
-        distance = math.sqrt(math.pow(normal_distance_vector,2) - math.pow(distance_x,2) - math.pow(distance_y,2))
-        """
-        # a 3d vector of the relative [x y z] location between the object and the camera (in meters)
-        distance = rotation_matrix.dot(rel.T).flatten() + offset
-        data_dictionary['distance'] = distance
+        logging.info("distance_vector_x " + str(distance[0]))
+        logging.info("distance_vector_y " + str(distance[1]))
+        logging.info("distance_vector_z " + str(distance[2]))
 
         return data_dictionary
-
-    def create_rotation_matrix(self):
-        sin, cos = np.sin(self.yaw_angle), np.cos(self.yaw_angle)
-        rotation_matrix = np.array([[cos, 0, sin],
-                                    [0, 1, 0],
-                                    [-sin, 0, cos]])
-        sin, cos = np.sin(self.pitch_angle), np.cos(self.pitch_angle)
-        rotation_matrix = rotation_matrix.dot(np.array([[1, 0, 0],
-                                                        [0, cos, -sin],
-                                                        [0, sin, cos]]))
-        sin, cos = np.sin(self.roll_angle), np.cos(self.roll_angle)
-        rotation_matrix = rotation_matrix.dot(np.array([[cos, -sin, 0],
-                                                        [sin, cos, 0],
-                                                        [0, 0, 1]]))
-        return rotation_matrix