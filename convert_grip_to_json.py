17d308b7441ca5da73fa503a47aaa01c70368bb1
tzmoz <tzmoz123@gmail.com>
2021-01-11 15:06:51 +0200

    edit the convertor
    add documentation, organized the code flow and add content to functions

--- convert_grip_to_json.py
+++ convert_grip_to_json.py
@@ -54,9 +54,10 @@ def edit_json(json_file_name: str, modifiers_values: dict):
     for modifier, values in modifiers_values.items():
         try:
             properties = eval(f'{modifier}({properties}, {values})')
-        except Exception:
-            print(f'{modifier}: ' + str(NameError("modifier function's name is incorrect or invalid JSON file")))
-            # raise NameError("modifier function's name is incorrect or invalid JSON file")
+        except Exception as exception:
+            print(exception.__class__.__name__ + f'{modifier}: ' + str(
+                NameError("modifier function's name is incorrect( or not in the GRIP_NAME_TO_FRAMEWORK_FUNCTIONS"
+                          "dictionary or invalid JSON file")))
             continue
 
     # Write back to the JSON file
@@ -67,29 +68,90 @@ def edit_json(json_file_name: str, modifiers_values: dict):
 def name(opened_json_file: OrderedDict, grip_values: list):
     """
     Example for adding a new modifier
-    name = name of modifier as in the GRIP file with '_' instead of ' '
+    name = the function name
     json_file = the opened JSON file (as in the builtin function)
-    grip_values = the values read from the grip files
+    grip_values = the values read from the grip files for this section only
     """
     return opened_json_file
 
 
-def Resize_Image(opened_json_file: OrderedDict, grip_values: list):
-    """Change the Resize_Image Values in the JSON data"""
-    # Not found on our Modifiers but can be added
-    return opened_json_file
+def blur(opened_json_file: OrderedDict, grip_values: list):
+    """
+    Change the Blur Values in the JSON data
 
+    :param opened_json_file: the given json file opened as a OrderedDict
+    :type opened_json_file: OrderedDict
 
-def Blur(opened_json_file: OrderedDict, grip_values: list):
-    """Change the Blur Values in the JSON data"""
+    :param grip_values: the inserted values in the grip file for this action by order from first to last
+    :type grip_values: list
+
+    """
+
     # We have different system so we need to convert this value
+    # our system gets a kernel with multiple values instead of one as in the GRIP that is why we convert it
+    opened_json_file["modifiers"]["Blur"]["kernel"] = [grip_values[1], grip_values[1]]
+
     return opened_json_file
 
 
-def HSV_Threshold(opened_json_file: OrderedDict, grip_values: list):
-    """Change the HSV_Threshold Values in the JSON data"""
+def hsv_threshold(opened_json_file: OrderedDict, grip_values: list):
+    """
+    Change the HSV_Threshold Values in the JSON data
+
+    :param opened_json_file: the given json file opened as a OrderedDict
+    :type opened_json_file: OrderedDict
+
+    :param grip_values: the inserted values in the grip file for this action by order from first to last
+    :type grip_values: list
+
+    """
+
+    # change the Hue (H)
+    opened_json_file["modifiers"]["ColorThreshold"]["low_h"] = grip_values[0]
+    opened_json_file["modifiers"]["ColorThreshold"]["high_h"] = grip_values[1]
+
+    # change the Saturation (S)
+    opened_json_file["modifiers"]["ColorThreshold"]["low_s"] = grip_values[2]
+    opened_json_file["modifiers"]["ColorThreshold"]["high_s"] = grip_values[3]
+
+    # change the Value (V)
+    opened_json_file["modifiers"]["ColorThreshold"]["low_v"] = grip_values[4]
+    opened_json_file["modifiers"]["ColorThreshold"]["high_v"] = grip_values[5]
+
     return opened_json_file
 
+
+def mask(opened_json_file: OrderedDict, grip_values: list):
+    """
+    Change the HSV_Threshold (because the mask is in it) Values in the JSON data
+
+    :param opened_json_file: the given json file opened as a OrderedDict
+    :type opened_json_file: OrderedDict
+
+    :param grip_values: the inserted values in the grip file for this action by order from first to last
+    :type grip_values: list
+
+    """
+
+    # NOTE: once it is set it can not be change to False
+    #       to change it to False you must do it directly through the json file
+    opened_json_file["modifiers"]["ColorThreshold"]["high_v"] = True
+    return opened_json_file
+
+
+"""
+A global dictionary for the integreation from the GRIP naming convention to the wanted function
+in the key there is the name as it is in the GRIP (notice the there are '_' instead of ' ')
+in the value there is the function you want to use to edit the json file (notice that you need to create one above)
+
+see the first example in the dictionary for guidance
+"""
+GRIP_NAME_TO_FRAMEWORK_FUNCTIONS = {
+    "The_Name_In_Grip": "the_function_to_use_for_this_case",
+    "Blur": blur,
+    "HSV_Threshold": hsv_threshold,
+    "Mask": mask
+}
 
 if __name__ == '__main__':
     # Check if input files were received correctly
@@ -101,11 +163,13 @@ if __name__ == '__main__':
         xml_data = gf.read()
     # Convert the xml string to BeautifulSoup object
     xml = BeautifulSoup(xml_data)
+
     # Get dict of all values and name
     values = find_values(xml)
     # Clean and formatting the dictionary
     values = clean_and_format_dict(values)
-    # print(values)
+
     json_file = sys.argv[2]
+
     # Run on all the modifiers and Change in the JSON file
     edit_json(json_file, values)
