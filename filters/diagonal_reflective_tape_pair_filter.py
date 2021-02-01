094bd4b9e212892b9383118d1c6530d81879de19
tzmoz <tzmoz123@gmail.com>
2021-02-01 21:15:03 +0200

    add documentation and annotations

--- filters/diagonal_reflective_tape_pair_filter.py
+++ filters/diagonal_reflective_tape_pair_filter.py
@@ -1,4 +1,5 @@
 from filters.filter import Filter
+from numpy import ndarray
 import sys
 import cv2
 import calculation_utils
@@ -11,31 +12,17 @@ class DiagonalReflectiveTapePair(Filter):
     Matches to the 2019 requirements
     """
 
-    def __find_two_highest_points(self, contour):
-        """
-        WIP
-        :param contour:
-        :return:
-        """
-        rectangle = cv2.minAreaRect(contour)
-        points = cv2.boxPoints(rectangle)
-        higher_point = min(points, key=lambda point: point[1])
-        # points.remove(higher_point)
-        points = numpy_utils.remove(points, higher_point)
-        lower_point = min(points, key=lambda point: point[1])
-        return higher_point, lower_point
-
-    def filter(self, contours):
-        shortest_higher_distance = sys.maxint
+    def filter(self, contours: [ndarray]) -> [ndarray]:
+        shortest_higher_distance = sys.maxsize
         return_contours = []
 
         for contour1 in contours:
-            # find the two highest points
+            # Find the two highest points
             higher_point_1, lower_point_1 = self.__find_two_highest_points(contour1)
             for contour2 in contours:
-                # find the two highest points
+                # Find the two highest points
                 higher_point_2, lower_point_2 = self.__find_two_highest_points(contour2)
-                # calculate the distances
+                # Calculate the distances
                 higher_distance = calculation_utils.distance(higher_point_1, higher_point_2)
                 lower_distance = calculation_utils.distance(lower_point_1, lower_point_2)
 
@@ -45,3 +32,25 @@ class DiagonalReflectiveTapePair(Filter):
                     shortest_higher_distance = higher_distance
 
         return return_contours
+
+    @staticmethod
+    def __find_two_highest_points(contour: ndarray) -> (ndarray, ndarray):
+        """
+        Find the two highest points in a given contour
+
+        :param contour: A contour to check on
+        :type contour: ndarray
+
+        :return: The two highest points
+        :rtype: (ndarray, ndarray)
+        """
+        rectangle = cv2.minAreaRect(contour)
+        points = cv2.boxPoints(rectangle)
+
+        higher_point = min(points, key=lambda point: point[1])
+        # points.remove(higher_point)
+        points = numpy_utils.remove(points, higher_point)
+
+        lower_point = min(points, key=lambda point: point[1])
+        print(type(higher_point))
+        return higher_point, lower_point
