8c69b6fbf364283588ccddcda3f4b32ec24374e1
tzmoz <tzmoz123@gmail.com>
2021-02-01 17:21:20 +0200

    add documentation and annotations

--- modifiers/modifier.py
+++ modifiers/modifier.py
@@ -1,14 +1,16 @@
 from abc import ABCMeta, abstractmethod
+from numpy import ndarray
 
 
 class Modifier(object):
     __metaclass__ = ABCMeta
 
     @abstractmethod
-    def modify(self, picture):
+    def modify(self, frame: ndarray) -> ndarray:
         """
-        :param picture: A picture to modify
-        :type picture: matrix of pixels
-        :rtype: matrix of pixels
+        :param frame: A picture to modify
+        :type frame: matrix of pixels (numpy.ndarray)
+
+        :rtype: matrix of pixels (numpy.ndarray)
         """
         pass
