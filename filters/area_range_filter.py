3fcfde24d97c8db2ca485c26edb1ae8cfa2f24d4
tzmoz <tzmoz123@gmail.com>
2021-02-01 21:14:41 +0200

    add documentation and annotations

--- filters/area_range_filter.py
+++ filters/area_range_filter.py
@@ -1,4 +1,5 @@
 from filters.filter import Filter
+from numpy import ndarray
 import cv2
 
 
@@ -8,24 +9,32 @@ class AreaRangeFilter(Filter):
     The area is defined by the contour area
     """
 
-    def __init__(self, min_area, max_area):
+    min_area: float
+    max_area: float
+
+    def __init__(self, min_area: float, max_area: float):
         """
         :param min_area: The minimum area of the contour to filter
+        :type min_area: float
+
         :param max_area: The maximum area of the contour to filter
+        :type max_area: float
         """
         self.min_area = min_area
         self.max_area = max_area
 
-    def __check_area_range(self, contour):
+    def __check_area_range(self, contour: ndarray) -> bool:
         """
         Checks if the contour area is in the desired range
+
         :param contour: A contour to check
-        :type contour: contour
+        :type contour: ndarray
+
         :return: True or False
         :rtype: boolean
         """
         area = cv2.contourArea(contour)
         return self.min_area <= area <= self.max_area
 
-    def filter(self, contours):
+    def filter(self, contours: [ndarray]) -> [ndarray]:
         return list(filter(self.__check_area_range, contours))
