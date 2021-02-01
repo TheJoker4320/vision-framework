d2d7dd2862aa1d90f99f05aabc9422201c8393f8
tzmoz <tzmoz123@gmail.com>
2021-02-01 19:36:52 +0200

    add documentation and annotations

--- extractors/simple_extractor.py
+++ extractors/simple_extractor.py
@@ -1,4 +1,5 @@
 from extractors.extractor import Extractor
+from numpy import ndarray
 import cv2
 
 
@@ -7,5 +8,5 @@ class SimpleExtractor(Extractor):
     Extracts all the contours that were detected at the given frame
     """
 
-    def extract(self, image):
-        return cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[1]
+    def extract(self, frame: ndarray) -> [ndarray]:
+        return cv2.findContours(frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[1]
