c04e04d5869cc39ae40543c3bb18a035dbb3ddd2
tzmoz <tzmoz123@gmail.com>
2021-02-02 17:33:06 +0200

    add documentation and annotations

--- calculations/distance_calculation_by_function.py
+++ calculations/distance_calculation_by_function.py
@@ -1,37 +1,41 @@
 from calculations.calculation import Calculation
+from numpy import ndarray
 import cv2
 import calculation_utils
 
 
 class DistanceCalculationByFunction(Calculation):
     """
-    Calculates the distance between the camera and the object
+    Calculates the distance between the camera and the object.
     Uses self made distance calculation function
          where f(x) is the real distance of the camera from the object
-         and x is the image's height in pixels (imaginary_height)
+         and x is the image's height in pixels (imaginary_height).
     """
 
-    def __init__(self, distance_function):
+    distance_function: str
+
+    def __init__(self, distance_function: str):
         """
-        :param distance_function: the distance function where x is represented as imaginary_height
+        :param distance_function: self made distance calculation function, where f(x) is the real distance of the
+                                  camera from the object and x is the image's height in pixels (imaginary_height).
         :type distance_function: str
         """
         self.distance_function = distance_function
 
-    def calc(self, contours):
+    def calc(self, contours: [ndarray]) -> dict:
         data_dictionary = {}
 
-        # merge all contours and refers to them as one
+        # Merge all contours and refers to them as one
         merged_cont = calculation_utils.merge_contours(contours)
 
-        # block the merged contour with a rectangle
+        # Block the merged contour with a rectangle
         rectangle = cv2.minAreaRect(merged_cont)
-        # get it's points
+        # Get it's points
         _, p1, p2, _ = cv2.boxPoints(rectangle)
-        # calculate the height of the rectangle (in pixels)
+        # Calculate the height of the rectangle (in pixels)
         imaginary_height = calculation_utils.distance(p1, p2)
 
-        # set the imaginary_height in the given function (as an x)
+        # Set the imaginary_height in the given function (as an x)
         distance = eval(self.distance_function, {}, {"imaginary_height": imaginary_height})
 
         data_dictionary['distance'] = distance
